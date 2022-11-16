9ec0633d0d7adc51987e70314c1dd433d74408d1
cesarb03 <cesar_b_03@outlook.com>
2022-11-16 15:58:34 -0300

    Entrega final V1.2 - Edit README

--- src/db/config.ts
+++ src/db/config.ts
@@ -8,15 +8,11 @@ const persistence = process.argv[2] || process.env.PERSISTENCE || 1;
 
 export const config = {
   ENVIRONMENT_MODE: process.env.ENVIRONMENT_MODE || 'development',
-  HOST: process.env.HOST || 'localhost',
   PORT: process.env.PORT || '8080',
   PERSISTENCE: persistence,
 
   MONGODB: process.env.MONGODB,
   SECRET_KEY: process.env.SECRET_KEY,
-
-  // SESSION_TIME: args.sessionTime || 60 * 100,
-  // SESSION_SECRET: process.env.SESSION_SECRET || 'secret',
 
   GMAIL_USER: process.env.GMAIL_USER,
   GMAIL_PASS: process.env.GMAIL_PASS,
@@ -29,14 +25,3 @@ export const config = {
 
   SESSION_TIME: process.env.SESSION_TIME,
 };
-
-// const persistence = process.argv[1] || process.env.PERSISTENCE || 1;
-// const persistence = 1;
-
-// const persistenceConfig = {
-//   MONGODB: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-
-//   PERSISTENCE: persistence,
-// };
-
-// export default persistenceConfig;
